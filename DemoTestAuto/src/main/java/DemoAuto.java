75f3cf1ef284a4fec79a4ab511f78e084089d5e9
haifa100990 <haifa.torjeman@gmail.com>
2020-11-29 13:29:36 +0100

    this is haifa's commit

--- DemoTestAuto/src/main/java/DemoAuto.java
+++ DemoTestAuto/src/main/java/DemoAuto.java
@@ -2,10 +2,9 @@
 public class DemoAuto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		
-		System.out.println("Création d'un compte");
+		System.out.println("Hello");
 	}
 
 }
